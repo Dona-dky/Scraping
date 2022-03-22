baa7ae953e5e88ac0eb61e06a5ed706f0b6688eb
Dona <dona.denkey_dandjein@edu.devinci.fr>
2022-03-22 23:55:54 +0100

    add 2nd tab

--- project/League_of_legends.py
+++ project/League_of_legends.py
@@ -1,3 +1,5 @@
+# import libraries
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -18,31 +20,58 @@ def get_list_of_champions_beginning():
     soup = get_list_of_champions_page()
 
     data = {}
-    #Tout le contenu du site
+    # All the content of the site
     all_content = soup.find(class_="mw-parser-output")
 
+    # h1 of the site
     mon_h1 = soup.h1.get_text()
     data['h1'] = mon_h1
 
+    # See also
     see_also = all_content.findChild(class_="dablink")
     see_also_recup = see_also.get_text()
     data['More'] = see_also_recup
 
+    # Description
     content_champions = see_also.find_next_sibling()
     content_champions_recup = content_champions.get_text().replace('\n', ' ')
     data['Description'] = content_champions_recup
 
-    #1er tableau -> class toc
-    first_board = content_champions.find_next_sibling()
+    # 1st tab -> class toc
+    first_tab = content_champions.find_next_sibling()
 
-    #titre du 1er tableau
-    title_first_board = first_board.findChild(class_="toctitle")
-    title_first_board_recup = title_first_board.h2.get_text()
-    data['Title_first_board'] = title_first_board_recup
+    # Title of 1st tableau
+    title_first_tab = first_tab.findChild(class_="toctitle")
+    title_first_tab_recup = title_first_tab.h2.get_text()
+    data['Title_first_tab'] = title_first_tab_recup
 
-    #données du tableau
-    list_board = title_first_board.find_next_sibling().text.replace('\n', ' ')
-    data['list_board'] = list_board
+    # Data of tab
+    list_tab = title_first_tab.find_next_sibling().text.replace('\n', ' ')
+    data['list_tab'] = list_tab
+
+    # Titre Available Champions
+    h2_2_tab = first_tab.find_next_sibling()
+    h2_2_tab_recup = h2_2_tab.get_text()
+    data['h2 2nd tab'] = h2_2_tab_recup
+
+    # 2eme tableau
+    table = h2_2_tab.find_next_sibling().find('tbody')
+    rows = table.find_all('tr')
+    for row in rows:
+        #if len(row) > 1:
+        columns = row.find_all('th')
+        cells = row.find_all('td')
+        print(columns)
+        print(cells)
+
+        # for rows in table_body('tr'):
+        # if len(rows) > 1:
+        # column = rows.th.get_text()
+        # cells = rows.td.get_text()
+        # cells = cells.replace('\t', '').replace('\n', ' ')
+        # print(cells)
+        # data[column] = cells
+        # data['name'] = h2_2_tab
 
     return data
 
